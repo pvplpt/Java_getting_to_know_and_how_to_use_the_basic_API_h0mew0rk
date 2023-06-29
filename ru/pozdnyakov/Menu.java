489eeb949513fbe17ba17e79ed4e0086c180ff29
Pavel Pozdnyakov <pvplpt@inbox.ru>
2023-06-29 21:34:17 +0500

    changed Menu

--- ru/pozdnyakov/Menu.java
+++ ru/pozdnyakov/Menu.java
@@ -4,17 +4,28 @@ import ru.pozdnyakov.lesson1.exercise1.Program1;
 import ru.pozdnyakov.lesson1.exercise2.Program2;
 import ru.pozdnyakov.lesson1.exercise3.Program3;
 import ru.pozdnyakov.lesson1.exercise4.Program4;
+/*
+import ru.pozdnyakov.lesson2.exercise1.Program21;
+import ru.pozdnyakov.lesson2.exercise2.Program22;
+import ru.pozdnyakov.lesson2.exercise3.Program23;
+import ru.pozdnyakov.lesson2.exercise4.Program24;
+ */
+
 
 
 public class Menu {
     public static void main(String[] args) {
         boolean flag = true;
         while (flag) {
-            System.out.println("\n\n1. Задание 1");
+            System.out.println("\n\nУРОК 1\n1. Задание 1");
             System.out.println("2. Задание 2");
             System.out.println("3. Задание 3");
             System.out.println("4. Задание 4");
-            System.out.println("5. Завершение работы приложения");
+            System.out.println("УРОК 2\n5. Задание 1");
+            System.out.println("6. Задание 2");
+            System.out.println("7. Задание 3");
+            System.out.println("8. Задание 4");            
+            System.out.println("9. Завершение работы приложения");
             int num = MyUtilities.inputNaturelNumber("Введите номер задания: ");
 
             switch (num) {
@@ -30,7 +41,20 @@ public class Menu {
                 case 4:
                     Program4.task4();
                     break;
-                case 5:
+/*                case 5:
+                    Program21.task1();
+                    break;
+                case 6:
+                    Program22.task2();
+                    break;
+                case 7:
+                    Program23.task3();
+                    break;
+                case 8:
+                    Program24.task4();
+                    break;                    
+*/
+                case 9:
                     flag = false;
                     System.out.println("\nДо новых встреч!");
                     System.out.println("Завершение работы приложения.");
