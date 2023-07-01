c5dddee47b24b53b57facd30469eb9865ae01014
Pavel Pozdnyakov <pvplpt@inbox.ru>
2023-07-02 01:32:40 +0500

    added lesson 3

---
+++ ru/pozdnyakov/lesson3/exercises/Program31.java
@@ -0,0 +1,20 @@
+/*
+ * Задание
+ * Пусть дан произвольный список целых чисел.
+ * 
+ * 1) Нужно удалить из него чётные числа
+ * 2) Найти минимальное значение
+ * 3) Найти максимальное значение
+ * 4) Найти среднее значение
+ */
+package ru.pozdnyakov.lesson3.exercises;
+
+public class Program31 {
+    public static void main(String[] args) {
+        task1234();
+    }
+
+    public static void task1234() {
+
+    }
+}
