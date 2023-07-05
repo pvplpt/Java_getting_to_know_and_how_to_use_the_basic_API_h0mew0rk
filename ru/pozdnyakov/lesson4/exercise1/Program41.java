18cd2582e1b82c7fc6ec22e4244bc175d3b001e5
Pavel Pozdnyakov <pvplpt@inbox.ru>
2023-07-05 23:48:42 +0500

    changed solution to task 1 from lesson 4

--- ru/pozdnyakov/lesson4/exercise1/Program41.java
+++ ru/pozdnyakov/lesson4/exercise1/Program41.java
@@ -11,27 +11,48 @@ import ru.pozdnyakov.MyUtilities;
 public class Program41 {
     static final int MINRANDOMVALUE = -100;
     static final int MAXRANDOMVALUE = 100;
+
     public static void main(String[] args) {
         task1();
     }
 
     public static void task1() {
-        LinkedList<Integer> linkedList = MyUtilities.createRandomLinkedList(MyUtilities.randomIntNumber(5, 50), MINRANDOMVALUE, MAXRANDOMVALUE);
+        LinkedList<Integer> linkedList = MyUtilities.createRandomLinkedList(MyUtilities.randomIntNumber(5, 50),
+                MINRANDOMVALUE, MAXRANDOMVALUE);
+
         System.out.println("\nИсходный список: ");
         System.out.println(linkedList);
 
-        LinkedList<Integer> rollLinkedList = reverseLinkedList(linkedList);
+        // LinkedList<Integer> rollLinkedList = reverseLinkedList(linkedList);
+        reverseLinkedList(linkedList);
 
         System.out.println("\nПеревернутый” список");
-        System.out.println(rollLinkedList);
-        
+        // System.out.println(rollLinkedList);
+        System.out.println(linkedList);
+
+    }
+    /*
+     * static LinkedList<Integer> reverseLinkedList(LinkedList<Integer> linkedList)
+     * {
+     * LinkedList<Integer> result = new LinkedList<>();
+     * while (linkedList.size() > 0) {
+     * result.add(linkedList.pollLast());
+     * }
+     * return result;
+     * }
+     */
+
+    private static void reverseLinkedList(LinkedList<Integer> linkedList) {
+        int size = linkedList.size();
+        int n = size / 2;
+        for (int i = 0; i < n; i++) {
+            int j = size - 1 - i;
+            Integer numberFromTheEnd = linkedList.remove(j);
+            Integer symmetricalNumber = linkedList.remove(i);
+            linkedList.add(i, numberFromTheEnd);
+            linkedList.add(j, symmetricalNumber);
+
+        }
     }
 
-    static LinkedList<Integer> reverseLinkedList(LinkedList<Integer> linkedList) {
-        LinkedList<Integer> result = new LinkedList<>();  
-        while (linkedList.size() > 0) {
-            result.add(linkedList.pollLast());
-        }
-        return result;
-    }
 }
