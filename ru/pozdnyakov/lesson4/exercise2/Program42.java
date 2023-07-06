8f15d3aff36530c5cf11a34c1d2b29285004c863
Pavel Pozdnyakov <pvplpt@inbox.ru>
2023-07-06 22:49:06 +0500

    added solution to task 2 from lesson 4

--- ru/pozdnyakov/lesson4/exercise2/Program42.java
+++ ru/pozdnyakov/lesson4/exercise2/Program42.java
@@ -21,11 +21,29 @@ package ru.pozdnyakov.lesson4.exercise2;
  * }
  */
 
+
 public class Program42 {
     public static void main(String[] args) {
         task2();
     }
 
     public static void task2() {
+        MyQueue myQueue = new MyQueue();
+        System.out.println("\nОчередь при открытии магазина:");
+        myQueue.printMyQueue();
+        myQueue.enqueue("Покупатель1");
+        myQueue.enqueue("Покупатель2");
+        myQueue.enqueue("Покупатель3");
+        myQueue.enqueue("Покупатель4");     
+        myQueue.enqueue("Покупатель5");    
+        System.out.println("\nОчередь через 20 минут:");        
+        myQueue.printMyQueue();
+        System.out.printf("\nРасплатился %s\n",myQueue.dequeue());
+        System.out.println("\nОстались:");
+        myQueue.printMyQueue();
+        System.out.printf("\nПервый в оереди %s\n",myQueue.first());
+        System.out.printf("\nРасплатился %s\n",myQueue.dequeue());
+        System.out.println("\nОстались:");
+        myQueue.printMyQueue();
     }
 }
