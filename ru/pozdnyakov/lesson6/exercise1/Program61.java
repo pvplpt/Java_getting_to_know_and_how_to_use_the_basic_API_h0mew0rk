963b8c2e180b36f38b5d777ebcc137deb0820326
Pavel Pozdnyakov <pvplpt@inbox.ru>
2023-07-20 22:42:31 +0500

    added lesson 6

---
+++ ru/pozdnyakov/lesson6/exercise1/Program61.java
@@ -0,0 +1,11 @@
+package ru.pozdnyakov.lesson6.exercise1;
+
+public class Program61 {
+    public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1() {
+        
+    }
+}
